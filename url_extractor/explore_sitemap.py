6fcf36f2a50a10e45f673808486e8dc94f3f9b30
zcabbub <andrei.bubutanu.19@gmail.com>
2020-12-18 15:13:50 +0200

    feature: Adding 'splitting' functionality to url_extractor.

--- url_extractor/explore_sitemap.py
+++ url_extractor/explore_sitemap.py
@@ -1,4 +1,6 @@
-#Import selenium
+# Import selenium
+import io
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from os import mkdir, listdir, remove
@@ -8,65 +10,83 @@ import time
 import xml.etree.ElementTree as etree
 import csv
 
+
 # Helper functions
 def get_counts_url(url):
-        first_part = 'https://gateway.gofundme.com/web-gateway/v1/feed/'
-        last_part = '/' + 'counts'
+    first_part = 'https://gateway.gofundme.com/web-gateway/v1/feed/'
+    last_part = '/' + 'counts'
 
-        middle_part = url.replace('https://www.gofundme.com/f/', '')
+    middle_part = url.replace('https://www.gofundme.com/f/', '')
 
-        api_url = first_part + middle_part + last_part
+    api_url = first_part + middle_part + last_part
 
-        return api_url
+    return api_url
 
-root = listdir()
-if "sitemaps" in root:
-    shutil.rmtree("sitemaps/")
-mkdir("sitemaps/")
 
-#To launch in headfull mode, comment out these options
-options = ChromeOptions()
-# NEEDS TO NOT BE HEADLESS
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--log-level=3")  # fatal
+def write(list_chunks, html):
+    if html:
+        for part in range(0, 3):
+            with io.open('urls/html_urls_{part}.txt'.format(part=part), 'w', encoding="utf-8") as f:
+                f.writelines('\n'.join(list_chunks[part]))
+    else:
+        for part in range(0, 3):
+            with io.open('urls/counts_urls_{part}.txt'.format(part=part), 'w', encoding="utf-8") as f:
+                f.writelines('\n'.join(list_chunks[part]))
 
-options.add_experimental_option("prefs",  {
-    "download.default_directory": r"C:\Users\UCL053368\Documents\Research\Enrico\Scraper\url_extractor\sitemaps",
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "safebrowsing.enabled": True
-    })
 
-options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return list(a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
-print('Starting to SCRAPE the sitemap...')
-DRIVER_PATH = 'chromedriver.exe'
-driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-
-driver.get("https://www.gofundme.com/sitemap.xml")
-driver.maximize_window()
-
-sitemap_lastMod = driver.find_element_by_tag_name("lastmod").get_attribute("innerHTML")
-
-xmlGzLocations = driver.find_elements_by_tag_name("loc")
-xmlGzLinks = []
-for location in xmlGzLocations:
-    text = location.get_attribute("innerHTML")
-    if text != "https://www.gofundme.com/sitemaps/sitemap_marketing.xml.gz":
-        xmlGzLinks.append(text)
-
-print('Downloading the .gz archived files...')
-for link in xmlGzLinks:
-    driver.get(link)
-
-time.sleep(5)
-driver.close()
+#
+# root = listdir()
+# if "sitemaps" in root:
+#     shutil.rmtree("sitemaps/")
+# mkdir("sitemaps/")
+#
+# # To launch in headfull mode, comment out these options
+# options = ChromeOptions()
+# # NEEDS TO NOT BE HEADLESS
+# options.add_argument("--window-size=1920,1080")
+# options.add_argument("--log-level=3")  # fatal
+#
+# options.add_experimental_option("prefs", {
+#     "download.default_directory": r"C:\Users\UCL053368\Documents\Research\Enrico\Scraper\url_extractor\sitemaps",
+#     "download.prompt_for_download": False,
+#     "download.directory_upgrade": True,
+#     "safebrowsing.enabled": True
+# })
+#
+# options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+#
+# print('Starting to SCRAPE the sitemap...')
+# DRIVER_PATH = 'chromedriver.exe'
+# driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+#
+# driver.get("https://www.gofundme.com/sitemap.xml")
+# driver.maximize_window()
+#
+# sitemap_lastMod = driver.find_element_by_tag_name("lastmod").get_attribute("innerHTML")
+#
+# xmlGzLocations = driver.find_elements_by_tag_name("loc")
+# xmlGzLinks = []
+# for location in xmlGzLocations:
+#     text = location.get_attribute("innerHTML")
+#     if text != "https://www.gofundme.com/sitemaps/sitemap_marketing.xml.gz":
+#         xmlGzLinks.append(text)
+#
+# print('Downloading the .gz archived files...')
+# for link in xmlGzLinks:
+#     driver.get(link)
+#
+# time.sleep(5)
+# driver.close()
 
 print('Extracting the HTML urls')
 html_urls = []
 sitemaps = listdir("sitemaps/")
 for sitemap in sitemaps:
-    with gzip.open("sitemaps/"+sitemap, "r") as gzXmlFile:
+    with gzip.open("sitemaps/" + sitemap, "r") as gzXmlFile:
         xml = etree.parse(gzXmlFile)
         gzXmlFile.close()
 
@@ -82,12 +102,15 @@ print('Creating the COUNTS urls')
 counts_urls = [get_counts_url(url) for url in html_urls]
 print('Done')
 
-print('Writing HTML urls list to "html_urls.txt"...')
-with open('urls/html_urls.txt', 'w') as file:
-    file.writelines('\n'.join(html_urls))
+print('Splitting the urls...')
+html_chunks = split(html_urls, 3)
+counts_chunks = split(counts_urls, 3)
 print('Done.')
 
-print('Writing COUNTS urls list to "counts_urls.txt"...')
-with open('urls/counts_urls.txt', 'w') as file:
-    file.writelines('\n'.join(counts_urls))
+print('Writing HTML urls...')
+write(html_chunks, True)
+print('Done.')
+
+print('Writing COUNTS urls...')
+write(counts_chunks, False)
 print('Done.')
